bf0ee54cf6dc559ad5ee000ab6eab729fa2d66ad
Gilberto Maia <antoniogilbertodasilva982@yahoo.com>
2020-12-05 21:34:15 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/antoniogilberto/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,13 @@
+package com.antoniogilberto.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.antoniogilberto.cursomc.domain.ItemPedido;
+
+@Repository 
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+	
+	
+
+}
