6f19192843dcd53b6d7466235b6294e1947c6849
Gilberto Maia <antoniogilbertodasilva982@yahoo.com>
2020-12-12 21:41:28 -0300

    Atualização: utilizando somente JsonIgnore

--- src/main/java/com/antoniogilberto/cursomc/domain/Produto.java
+++ src/main/java/com/antoniogilberto/cursomc/domain/Produto.java
@@ -31,7 +31,7 @@ public class Produto implements Serializable{
 	private Double preco;
 	
 	
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA", 
 	      joinColumns = @JoinColumn(name = "produto_id"), 
